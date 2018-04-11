9f80e0f9d4ecd42f95e6b65e471063ae2faa4f57
iyustlop <iyustlop@gmail.com>
2018-04-11 09:07:11 +0200

    Due to Compatibility problem between Ubuntu and chrome. I chose change to FireFox. Removed comments in feature file also.

--- src/test/java/es/tuenti/qa/test/SeleniumSteps.java
+++ src/test/java/es/tuenti/qa/test/SeleniumSteps.java
@@ -1,26 +1,44 @@
 package es.tuenti.qa.test;
 
+import cucumber.api.Scenario;
 import cucumber.api.java8.En;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import sun.security.util.PendingException;
+
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumSteps implements En {
 
+    private static final String GECKO_DRIVER = "src/test/resources/driver/geckodriver";
+    private static final String URL = "http://www.tuenti.es";
     private String phoneNumber;
     private String password;
+    private String userGoogleEmail;
+    private String passwordGoogleEmail;
+    private String userTwitterEmail;
+
+    WebDriver driver;
 
 
     public SeleniumSteps() {
 
-        Given("^Having a correct phone number and$", () -> phoneNumber = "684262289");
+        Before((Scenario scenario) -> {
+            System.setProperty("webdriver.gecko.driver", GECKO_DRIVER);
+            driver = new FirefoxDriver();
 
-        Given("^Having a correct password and$", () -> password = "testtest");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
 
-        Given("^Having the home page open and #Web=www\\.tuenti\\.es; Android=open App\\.$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
         });
 
-        When("^the user clicks on the sign in link and # Web=entrar; Android=inicia sesion\\.$", () -> {
+        Given("^Having a correct phone number$", () -> phoneNumber = "684262289");
+
+        Given("^Having a correct password$", () -> password = "testtest");
+
+        Given("^Having the home page open$", () -> driver.get(URL));
+
+        When("^the user clicks on the sign in link  # Web=entrar; Android=inicia sesion\\.$", () -> {
             // Write code here that turns the phrase above into concrete actions
             throw new PendingException();
         });
@@ -40,20 +58,11 @@ public class SeleniumSteps implements En {
             throw new PendingException();
         });
 
-        Given("^Having a valid Google user email and$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        });
+        Given("^Having a valid Google user email$", () -> userGoogleEmail = "ops_internacional+24@tuenti.com");
 
-        Given("^Having a valid Google password and$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        });
+        Given("^Having a valid Google password$", () -> passwordGoogleEmail = "testtest");
 
-        Given("^Having the home page open$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        });
+
 
         When("^the user clicks on the sign in link and$", () -> {
             // Write code here that turns the phrase above into concrete actions
@@ -125,15 +134,16 @@ public class SeleniumSteps implements En {
             throw new PendingException();
         });
 
-        Given("^Having a correct phone number$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        });
 
         Given("^Having a wrong password and$", () -> {
             // Write code here that turns the phrase above into concrete actions
             throw new PendingException();
         });
 
+        After((Scenario scenario) -> {
+           driver.quit();
+        });
+
     }
+
 }
--- src/test/resources/es/tuenti/qa/test/SeleniumTest.feature
+++ src/test/resources/es/tuenti/qa/test/SeleniumTest.feature
@@ -2,19 +2,19 @@ Feature: Selenium Test
   The following scenarios test the login access.
 
   Scenario: Happy path
-    Given Having a correct phone number and
-    And Having a correct password and
-    And Having the home page open and #Web=www.tuenti.es; Android=open App.
-    When the user clicks on the sign in link and # Web=entrar; Android=inicia sesion.
-    When those data are introduced in the login page
-    When the access button is clicked #Web=Entrar en tu cuenta; Android=entrar
+    Given Having a correct phone number
+    And Having a correct password
+    And Having the home page open
+    When the user clicks on the sign in link
+    And those data are introduced in the login page
+    And the access button is clicked
     Then the user access to the home page
 
   Scenario: Happy path, Access with Google user
-    Given Having a valid Google user email and
-    And Having a valid Google password and
+    Given Having a valid Google user email
+    And Having a valid Google password
     And Having the home page open
-    When the user clicks on the sign in link and
+    When the user clicks on the sign in link
     When the user clicks on the Google Icon
     When those data are introduced in the Google login page
     Then the user access to the home page
@@ -39,7 +39,7 @@ Feature: Selenium Test
 
   Scenario: Wrong phone number.
     Given Having a wrong phone number
-    And Having a correct password and
+    And Having a correct password
     And Having the home page open
     When the user clicks on the sign in link and
     When those data are introduced in the login page
@@ -47,8 +47,8 @@ Feature: Selenium Test
 
   Scenario: Wrong password.
     Given Having a correct phone number
-    And Having a wrong password and
+    And Having a wrong password
     And Having the home page open
-    When the user clicks on the sign in link and
+    When the user clicks on the sign in link
     When those data are introduced in the login page
     Then a message shall appear suggesting some data are wrong
